414b7dee56800703a4fcda75ac1ddf21cc00c318
Kakdeayush <kakdeayush376@gmail.com>
2026-07-03 15:48:51 +0530

    solved some new problms on the patterns

---
+++ src/pattern_programs/DiagonalPattern.java
@@ -0,0 +1,19 @@
+package pattern_programs;
+
+public class DiagonalPattern {
+public static void main(String[] args) {
+	int rows=5;
+	int columns=5;
+	
+	for(int i=1;i<=rows;i++) {
+		for(int j=1;j<=columns;j++) {
+			if(i==j) {
+				System.out.print("* ");
+			}else {
+				System.out.print("  ");
+			}
+		}
+		System.out.println();
+	}
+}
+}
---
+++ src/pattern_programs/StarBox.java
@@ -0,0 +1,15 @@
+package pattern_programs;
+
+public class StarBox {
+public static void main(String[] args) {
+	int row =5;
+	int column=5;
+	
+	for(int i=1;i<=5;i++) {
+		for(int j=0;j<=5;j++) {
+			System.out.print("* ");
+		}
+		System.out.println(8);
+	}
+}
+}
