e22fcd515855b89af355c3c828bb708926358661
Kakdeayush <kakdeayush376@gmail.com>
2026-07-03 09:44:23 +0530

    solved new problems

---
+++ src/pattern_programs/magic_box.java
@@ -0,0 +1,15 @@
+package pattern_programs;
+
+public class magic_box {
+public static void main(String[] args) {
+	int m=5;
+	int n=5;
+	
+	for(int i=1;i<=m;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print(i+" "+j+"  " );
+		}
+		System.out.println();
+	}
+}
+}
