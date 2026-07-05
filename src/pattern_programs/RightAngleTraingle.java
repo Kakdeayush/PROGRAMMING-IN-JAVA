c7ab947dc7a3ce2a4e80cbdf99ca12bc0ce71475
Kakdeayush <kakdeayush376@gmail.com>
2026-07-05 19:07:36 +0530

    solved a problem on right angle traingle

---
+++ src/pattern_programs/RightAngleTraingle.java
@@ -0,0 +1,20 @@
+package pattern_programs;
+
+public class RightAngleTraingle {
+public static void main(String[] args) {
+	int num=3;
+	
+	for(int i=1;i<num*2;i++) {
+		for(int j=1;j<num*2;j++) {
+			if(i>=j) {
+				System.out.print("* ");
+			}
+			else {
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+}
+}
